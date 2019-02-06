524841a2ccd28f52c01d87ec97726446961a4d49
James de Winton <james.dewinton@gmail.com>
2019-02-06 21:12:26 +0000

    migration for add food

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_06_123342) do
+ActiveRecord::Schema.define(version: 2019_02_06_133807) do
 
   create_table "larderdbs", force: :cascade do |t|
     t.string "food"
     t.text "nutrition"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", limit: 128, null: false
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128, null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
 end
