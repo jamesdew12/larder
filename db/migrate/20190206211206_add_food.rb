61f9392ed216f8373caf033608d7c13cef281a4e
James de Winton <james.dewinton@gmail.com>
2019-02-06 21:21:02 +0000

    table created for food

--- db/migrate/20190206211206_add_food.rb
+++ db/migrate/20190206211206_add_food.rb
@@ -1,4 +1,11 @@
 class AddFood < ActiveRecord::Migration[5.2]
-  def change
+    def change
+      create_table :food do |t|
+        t.string :name, null:false
+        t.text :description, null:false
+        t.belongs_to :user, null: false, foreign_key: true
+        t.timestamps null: false
+    end
+    add_index :food, [:user_id, :name]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_06_133807) do
+ActiveRecord::Schema.define(version: 2019_02_06_211206) do
+
+  create_table "food", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "name"], name: "index_food_on_user_id_and_name"
+    t.index ["user_id"], name: "index_food_on_user_id"
+  end
 
   create_table "larderdbs", force: :cascade do |t|
     t.string "food"
