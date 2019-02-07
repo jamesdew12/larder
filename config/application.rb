28f50178d797781d25c61a03e0029199e764ca08
James de Winton <james.dewinton@gmail.com>
2019-02-07 13:09:50 +0000

    new larder db routes added

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require_relative 'boot'
 
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -9,8 +10,9 @@ Bundler.require(*Rails.groups)
 module Larder
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-
+    config.generators do |g|
+      g.test_framework :minitest, spec: true
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
---
+++ test/models/food_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+describe Food do
+  it "should fail" do
+    assert true == false
+
+  end
+end
